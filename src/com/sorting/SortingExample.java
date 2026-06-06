16ccbf111b6a90c4a25850065c39a7a1eb6d5311
shreyakale08 <kaleshreya347@gmail.com>
2026-06-06 22:24:52 +0530

    Sorting example

---
+++ src/com/sorting/SortingExample.java
@@ -0,0 +1,15 @@
+package com.sorting;
+
+import java.util.Arrays;
+
+public class SortingExample {
+
+	public static void main(String[] args) {
+     
+		
+		int[]arr = {9, 4, 7, 1, 3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+	}
+
+}
